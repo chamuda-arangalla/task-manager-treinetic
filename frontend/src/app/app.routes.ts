fed79199d0be74add927f66519dbe4e10747da5e
chamuda-arangalla <chamudaarangalla@gmail.com>
2025-04-06 23:30:55 +0530

    Implement Task form UI and add route

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,10 +2,11 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { SignupComponent } from './pages/signup/signup.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { TaskFormComponent } from './pages/task-form/task-form.component';
 
 export const routes: Routes = [
     { path:'login',component:LoginComponent},
     { path:'signup',component:SignupComponent},
     { path:'dashboard',component:DashboardComponent},
-    // { path:'login',component:LoginComponent},
+    { path:'task-form',component:TaskFormComponent},
 ];
