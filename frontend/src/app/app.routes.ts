1b4e25ca3fba167a387cf2e969b25217e854607d
chamuda-arangalla <chamudaarangalla@gmail.com>
2025-04-06 23:04:58 +0530

    Set route for dashboard

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { SignupComponent } from './pages/signup/signup.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 export const routes: Routes = [
     { path:'login',component:LoginComponent},
     { path:'signup',component:SignupComponent},
-    // { path:'tasks',component:Task},
+    { path:'dashboard',component:DashboardComponent},
     // { path:'login',component:LoginComponent},
 ];
